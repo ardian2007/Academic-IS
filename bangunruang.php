228f40cf2711869227e9cd46887bbb9012f247e3
Ardiansyah <ardian2007@gmail.com>
2019-03-04 20:25:41 +0700

    tambah file baru

---
+++ bangunruang.php
@@ -0,0 +1,34 @@
+<?php
+class persegipanjang
+{
+	public $luas, $keliling;
+	
+	function luasPersegipanjang($panjang, $lebar){
+		return $this->luas = $panjang * $lebar;
+	}
+
+	function kelilingPersegipanjang(){
+		return $this->keliling = 2 * $this->luas;
+	}
+
+}
+
+class segitiga
+{
+	
+}
+
+class lingkaran
+{
+
+}
+
+class kubus
+{
+
+}
+
+class jajarangenjang
+{
+
+}
