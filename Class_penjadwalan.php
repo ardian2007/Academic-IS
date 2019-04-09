84388b31298c2f23f1550d5f31a06b959f6ef8e6
Adhymas Fajar Sudrajad <48277426+dmonh3h3@users.noreply.github.com>
2019-04-09 13:15:29 +0700

    Update Class_Penjadwalan.php bagian Adhymas Fajar S
    memperbaiki pengambilan data seluruh mahsasiwa berdasarkan nim mahasiswa
    bila mahasiswa memiliki jadwal (seminar proposal/Ujian Pendadaran) maka terambil semua data yg terhubung oleh nim dari beberapa tabel
    bila tidak maka akan mengambil data mahasiswa, dosen, dan dosen uji.

--- Class_penjadwalan.php
+++ Class_penjadwalan.php
@@ -2,31 +2,72 @@
 
 	require_once('database.php');
 
-class Penjadwalan{
-
+class Penjadwalan extends Database{ 
+	public function __construct(){
+		parent::__construct();
 	
-	// kalau function memakai parameter, silahkan langsung di isi parameter nya
-	// Mengambil Data Mahasiswa Dan 
-	public function getaMahasiswaDanDosenBimbinganByNim($nim)
+	}
+	// Menghirung Banyaknya Baris dari query yg dieksekusi
+	public function hitung_row($sql)
 	{
 		// dmonh3h3(Adhymas Fajar Sudrajad)
-		$query = "SELECT * FROM mahasiswa_metopen JOIN dosen on dosen.niy = mahasiswa_metopen.dosen"
-		$this->eksekusi($query);
-		return $this->result;
+		$result=mysqli_num_rows($sql);
+		return $result;
 	}
 	// Mengambil seluruh Data Mahasiswa Dan Jadwal
-	public function getDataALLMahasiswaByNim($nim)
-	{
+	public function getDataALLMahasiswaByNim($nim){
 		// dmonh3h3(Adhymas Fajar Sudrajad)
 		$query = "SELECT * FROM mahasiswa_metopen JOIN dosen on dosen.niy = mahasiswa_metopen.dosen 
 			JOIN penjadwalan ON penjadwalan.nim = mahasiswa_metopen.nim 
 			JOIN penguji ON penguji.id_jadwal = penjadwalan.id_jadwal 
-			JOIN dosen_penguji ON penguji.niy =  dosen_penguji.niy_dosen_penguji 
-			WHERE mahasiswa_metopen.nim=$nim"
-		$this->eksekusi($query);
-		return $this->result;
+			JOIN dosen_penguji ON penguji.niy=  dosen_penguji.niy_dosen_penguji
+			WHERE mahasiswa_metopen.nim=$nim";
+		//$sql=$this->eksekusi($query);
+		//  cek penguji dan mahasiswa
+		$sql=$this->eksekusi($query);
+		$hasil=$this->hitung_row($sql);
+		// cek Jadwal dan Mahasiswa
+		$sql2=$this->getDataPenjadwalanByNIM($nim);
+		$hasil_ceknim=$this->hitung_row($sql2);
+		if($hasil > 0 && $hasil_ceknim > 0){
+			$hasil=$this->eksekusi($query);
+			foreach ($hasil as $key) {	
+				$_SESSION['masuk']=1;
+				$_SESSION['nim']=$key['nim'];
+				$_SESSION['nama']=$key['nama'];
+				$_SESSION['topik']=$key['topik'];
+				$_SESSION['dosen']=$key['dosen'];
+				$_SESSION['niy']=$key['niy'];
+				$_SESSION['nama_dosen']=$key['nama_dosen'];
+				$_SESSION['email']=$key['email'];
+				$_SESSION['bidang_keahlian']=$key['bidang_keahlian'];
+				$_SESSION['id_jadwal']=$key['id_jadwal'];
+				$_SESSION['jenis_ujian']=$key['jenis_ujian'];
+				$_SESSION['tanggal']=$key['tanggal'];
+				$_SESSION['jam']=$key['jam'];
+				$_SESSION['tempat']=$key['tempat'];
+				$_SESSION['dosen_uji']=$key['niy_dosen_penguji'];
+				
+			}
+			return $hasil;
+		}
+		else{
+			$query = "SELECT * FROM mahasiswa_metopen JOIN dosen on dosen.niy = mahasiswa_metopen.dosen where mahasiswa_metopen.nim=$nim";
+			$hasil=$this->eksekusi($query);
+			foreach ($hasil as $key) {	
+				$_SESSION['masuk']=2;
+				$_SESSION['nim']=$key['nim'];
+				$_SESSION['nama']=$key['nama'];
+				$_SESSION['topik']=$key['topik'];
+				$_SESSION['dosen']=$key['dosen'];
+				$_SESSION['niy']=$key['niy'];
+				$_SESSION['nama_dosen']=$key['nama_dosen'];
+				$_SESSION['email']=$key['email'];
+				$_SESSION['bidang_keahlian']=$key['bidang_keahlian'];
+			}	
+			return $hasil;
+		}
 	}
-	
 	public function createIdJadwal($id, $penguji1, $penguji2, $tanggal, $jam, $tempat)
 	{
 		$id_jadwal 	= $id;
