2b5302692cce954e76bfb311c02f1336c9b463a5
kartodengkek@gmail.com <kartodengkek@gmail.com>
2019-04-11 21:32:45 +0700

    1700018158 Arifaleo Nurdin Fungsi mencari_data_log_melalui_kata_yang_ingin_dicari

--- Database.php
+++ Database.php
@@ -142,6 +142,15 @@
 			return $this->result;
 		}
 
+		//dibuat oleh Arifaleo Nurdin (1700018158)
+		public function mencari_data_log_melalui_kata_yang_ingin_dicari($nim, $materi_key)
+		{
+			$query = "SELECT materi_bimbingan AS Materi, tanggal_bimbingan AS Tanggal, jam AS Jam FROM mahasiswa_metopen JOIN skripsi ON mahasiswa_metopen.nim=skripsi.nim JOIN logbook_bimbingan ON skripsi.id_skripsi=logbook_bimbingan.id_skripsi WHERE mahasiswa_metopen.nim= $nim AND materi_bimbingan LIKE '%$materi_key%'";
+			$this->eksekusi($query);
+			return $this->result;
+		}
+
+
 
 
 
