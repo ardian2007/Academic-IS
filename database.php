ecf7fea840f16062b878096441d819fdea4f8df4
randiindraguna <randiindraguna2203@gmail.com>
2019-04-23 20:21:02 +0700

    UTS-Randi Indraguna-1700018156

--- database.php
+++ database.php
@@ -28,7 +28,7 @@ class Database
 		$this->pass="";				// variabel pass diisi kosong
 		$this->database="manajemen_skripsi_prpl";	// variabel diisi database yang digunakan yang ada dalam sever localhost yaitu manajemen_skripsi_prpl
 	}
-	//dibuat oleh agung parmono
+	//Dibuat oleh agung parmono
 	public function connect(){	//fungsi yang digunakan untuk koneksi ke database manajemen_skripsi_prpl
 		$this->conn=mysqli_connect($this->host,$this->user,$this->pass);	//menghubungkan ke localhost
 		mysqli_select_db($this->conn,$this->database);						//menghubungkan ke database
@@ -36,76 +36,78 @@ class Database
 			return die('Maaf, koneksi belum tersambung: '.mysqli_connect_error()); //Maaf,koneksi belum tersambung
 		}
 	}
-	//dibuat oleh agung parmono
+	//Dibuat oleh agung parmono
 	public function eksekusi($query){	//fungsi yang digunakan untuk eksekusi query yang ada
 		$this->result=mysqli_query($this->conn,$query);	//mengembalikan hasil dari query yang dieksekusi
 	}
-	//dibuat oleh agung parmono
+	//Dibuat oleh agung parmono
 	public function getDosen(){	//fungsi yang dibuat untuk menampilkan seluruh data dosen
 		$query="SELECT * FROM dosen";	//query untuk menampilkan seluruh data dosen
 		$this->eksekusi($query);		//mengeksekusi query diatas
 		return $this->result;			//mengembalikan hasil dari query diatas
 	}
-	//dibuat oleh ihsan fadhilah
+	//Dibuat oleh ihsan fadhilah
 	public function getMhs(){  //fungsi untuk menampilkan data mahasiswa
 		$query="SELECT mahasiswa_metopen.nama as nama,mahasiswa_metopen.nim as nim,mahasiswa_metopen.jenis_kelamin as jenis_kelamin,mahasiswa_metopen.topik as topik,dosen.nama as namados,mahasiswa_metopen.bidang_minat as bidang_minat, mahasiswa_metopen.tanggal_mulai as tanggal_mulai FROM mahasiswa_metopen JOIN dosen WHERE mahasiswa_metopen.dosen=dosen.niy"; //query untuk menampilkan nama,,nim,jenis kelamin, topik, nama dosen, bidang minat, dan tanggal mulai mahasiswa itu
 		$this->eksekusi($query);	//mengeksekusi query diatas
 		return $this->result;		//untuk mengembalikan hasil eksekusi fungsi
 	}
-	//dibuat oleh agung parmono
+	//Dibuat oleh agung parmono
 	public function register($nim,$nama,$jenis_kelamin,$topik,$dosen,$bidang_minat,$tanggal_mulai){	//fungsi yang digunakan untuk mendaftarkan mahasiswa untuk metopen
 		$query = "INSERT INTO mahasiswa_metopen(nim,nama,jenis_kelamin,topik,dosen,bidang_minat,tanggal_mulai) VALUES ('$nim','$nama','$jenis_kelamin','$topik','$dosen','$bidang_minat','$tanggal_mulai')"; //query untuk menambah data mahasiswa berupa nim,nama,jenis kelamin,topik,dosen,bidang minat dan tanggal mulai yang akan disimpan pada tabel mahasiswa_metopen
 		$this->eksekusi($query);	//mengeksekusi query diatas
 		return $this->result;		//mengembalikan hasil dari query diatas
 	}
-	//dibuat oleh shindi
+	//Dibuat oleh shindi
 	public function getJumlahMhs(){ //fungsi yang di gunakan untuk mengambil jumlah mahasiswa
 		$query="SELECT COUNT(nim) as jumlah_mahasiswa FROM mahasiswa_metopen"; //query untuk mengambil jumlah mahasiswa berdasarkan nim
 		$this->eksekusi($query); //mengeksekusi query diatas
 		return $this->result;  //mengembalikan hasil dari query diatas
 	}
-	//dibuat oleh aurelia gofarawati
+	//Dibuat oleh aurelia gofarawati
 	public function getJumlahDosen(){ //fungsi yang digunakan untuk mengambil jumlah dosen
 		$query="SELECT COUNT(niy) as jumlah_dosen FROM dosen"; //query untuk mengmbil jumlah dosen berdasarkan niy
 		$this->eksekusi($query); //mengeksekusi query diatas
 		return $this->result; //mengembalikan hasil dari query diatas
 	}
-	public function FormUpdateDataMahasiswaMetopen($nim){
-	//dibuat oleh randi indraguna
-		$query = "SELECT nim, nama, jenis_kelamin, topik, dosen, bidang_minat FROM mahasiswa_metopen where nim='$nim'";
-		$this->eksekusi($query);
-		return $this->result;
+	//Dibuat oleh randi indraguna
+	public function FormUpdateDataMahasiswaMetopen($nim){ //fungsi yang digunakan untuk menampilkan form update data mahasiswa metopen
+		$query = "SELECT nim, nama, jenis_kelamin, topik, dosen, bidang_minat 
+				FROM mahasiswa_metopen 
+				where nim='$nim'"; //query untuk menampilkan form update berdasarkan $nim yang akan di pilih untuk di update
+		$this->eksekusi($query); //untuk mengeksekusi query diatas
+		return $this->result; //mengembalikan hasil query diatas
 		
 	}
-	
-	public function UpdateDataMahasiswaMetopen($nim,$nama,$jenis_kelamin,$topik,$dosen,$bidang_minat){
-	//dibuat oleh randi indraguna
-		$query="UPDATE mahasiswa_metopen SET nim='$nim',nama='$nama',jenis_kelamin='$jenis_kelamin',topik='$topik',dosen='$dosen',bidang_minat='$bidang_minat' WHERE nim='$nim' ";
-		$this->eksekusi($query);
-		return $this->result;
+	//Dibuat oleh randi indraguna
+	public function UpdateDataMahasiswaMetopen($nim,$nama,$jenis_kelamin,$topik,$dosen,$bidang_minat){ //fungsi yang digunakan untuk mengupdate data mahasiswa metopen
+		$query="UPDATE mahasiswa_metopen SET nim='$nim',nama='$nama',jenis_kelamin='$jenis_kelamin',topik='$topik',
+				dosen='$dosen',bidang_minat='$bidang_minat' 
+				WHERE nim='$nim'"; //query untuk mengupdate data mahasiswa metopen yang telah diubah di form update 
+		$this->eksekusi($query); //untuk mengeksekusi query diatas
+		return $this->result; //mengembalikan hasil query diatas
 	}
+	//Dibuat oleh ihsan fadhilah
 	public function getJumlahMahasiswaBimbingan(){ //fungsi untuk mendapatkan jumlah mahasiswa bimbingan setiap dosennya 
-		//dibuat oleh ihsan fadhilah
 		$query = "SELECT dosen.nama as nama, dosen.niy as niy, COUNT(nim) as jumlah_mahasiswa FROM mahasiswa_metopen JOIN dosen ON mahasiswa_metopen.dosen = dosen.niy";  //query untuk mendapatkan jumlah mahasiswa bimbingan setiap dosennya
 		$this->eksekusi($query);	//berguna untuk mengeksekusi query sql diatas yang telah dibuat
 		return $this->result;		//untuk mengembalikan hasil eksekusi fungsi
 	}
-
+    //Dibuat oleh Nur Fadhilah Alfianty Firman
 	public function CariDataMahasiswa($nim){ // function digunakan untuk mempermudah mencari data mahasiswa
-		//dibuat oleh Nur Fadhilah Alfianty Firman
 		$query = "SELECT * FROM mahasiswa_metopen WHERE nim LIKE '%$nim'"; //digunakan untuk menampilkan data mahasiswa metopen dan mencari dengan nim
 		$query="SELECT mahasiswa_metopen.nama as nama,mahasiswa_metopen.nim as nim,mahasiswa_metopen.jenis_kelamin as jenis_kelamin,mahasiswa_metopen.topik as topik,dosen.nama as namados,mahasiswa_metopen.bidang_minat as bidang_minat, mahasiswa_metopen.tanggal_mulai as tanggal_mulai FROM mahasiswa_metopen JOIN dosen WHERE mahasiswa_metopen.dosen=dosen.niy";
 		$this->eksekusi($query); // berguna untuk mengeksekusi query sql diatas yang telah dibuat
 		return $this->result; //untuk mengembalikan hasil eksekusi fungsi 
 	}
+	//Dikerjakan oleh Eef Mekelliano
 	public function DeleteMahasiswaMetopen($nim){ // Fungsi ini untuk menghapus data mahasiswa metopen
-		// Dikerjakan oleh Eef Mekelliano
 		$query="DELETE FROM mahasiswa_metopen WHERE nim='$nim'"; // Query ini digunakan untuk menghapus data mahasiswa yang telah mendaftar metopen
 		$this->eksekusi($query); // untuk mengeksekusi query sql diatas yang telah dibuat
 		return $this->result; // untuk mengembalikan hasil eksekusi pada fungsi ini.
 	}
+	//Dikerjakan oleh amir fauzi ansharif
 	public function getDataMahasiswaBimbinganDosenTertentu($niy){ //fungsi ini digunakan untuk menampilkan data mahasiswa bimbingan dari dosen tertentu dengan paramater $niy 
-		//Dikerjakan oleh amir fauzi ansharif
 		$query="SELECT dosen.nama as nama_dosen, dosen.niy as niy, mahasiswa_metopen.nama as nama_mhs FROM dosen JOIN mahasiswa_metopen WHERE dosen.niy = '$niy'"; //query ini digunakan untuk menampilkan data mahasiswa bimbingan dari dosen tertentu
 		$this->eksekusi($query); //untuk mengeksekusi query diatas
 		return $this->result; //untuk mengembalikan hasil eksekusi query diatas
