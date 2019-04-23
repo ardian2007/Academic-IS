19d8cec03783fc97d89917aa804d82e36e3bbab8
EefMkll <mekelliano@gmail.com>
2019-04-23 16:24:26 +0700

    Saya telah mengubah UI semua file di register metopen

--- caridata.php
+++ caridata.php
@@ -15,71 +15,25 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title></title>
-  </head>
-  <body bgcolor="pink">
-    <table border="0" width="100%" height="20%">
-      <tr>
-        <td>
-          <img src="head.png" alt="logo" style="width:100%; height:30%; position: relative; z-index: 1; top: 0px;">
-          <b><h2 style="position: absolute; top: 25px; left: 72%; z-index: 2; color: pink; font-family: Cooper Black;">Manajemen Skripsi</h2>
-          <p style="position: absolute; top: 58px; left: 72%; z-index: 2; color: #F5F5F5; font-family: Times New Roman;">Website Proyek PRPL Kelas C</p></b>
-        </td>
-      </tr> 
-    </table>
+    <!-- /\ambil css penjadwalan -->
+    <!-- Tambahan CSS -->
+    <link rel="stylesheet" href="css/style_penjadwalan.css">
+    <link rel="stylesheet" href="css/switches_Penjadwalan.css">
 
-    <table border="0" width="100%" height="10%">
-      <tr>
-        <td>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand"><img src="logo.png" alt="Logo" style="width:60px;"></a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navb">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Metopen
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="input.php">Pendaftaran Metopen</a>
-                    <a class="dropdown-item" href="out.php">Data Mahasiswa Metopen</a>
-                    <a class="dropdown-item" href="out2.php">Data Dosen</a>
-                    <a class="dropdown-item" href="out2.php">Cari Data Mahasiwa</a>
-                  </div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Seminar Proposal</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Skripsi
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Bimbingan</a>
-                    <a class="dropdown-item" href="#">Penjadwalan</a>
-                    <a class="dropdown-item" href="#">Ujian Pendadaran</a>
-                  </div>
-                </li>
-              </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-outline-danger" type="button">Search</button>
-              </form>
-            </div>
-          </nav>
-        </td>
-      </tr> 
-    </table>
+    <style type="text/css" href="css/tombol_penjadwalan.css"></style>
+    <!--  -->
+
+
+    <title>Cari Data Mahasiswa</title>
+  </head>
+  <body bgcolor="yellow">
+    
+    <?php include 'templates/navbar_mhs.html'?>
 
     <table border="0" width="100%" height="100%" align="center">
-      <tr><td colspan="3" bgcolor="pink"><br></td></tr>
+      <tr><td colspan="3" bgcolor="yellow"><br></td></tr>
       <tr>
-        <td width="25%" bgcolor="pink" rowspan="2"></td>
+        <td width="25%" bgcolor="yellow" rowspan="2"></td>
         <td width="50%">
           <table cellpadding="20"width="100%" border="0"  height="100%">
             <tr>
@@ -94,14 +48,14 @@
             </tr>
           </table>
         </td>
-        <td width="25%" bgcolor="pink" rowspan="2"></td>
+        <td width="25%" bgcolor="yellow" rowspan="2"></td>
       </tr>
     </table>
 
     <table cellpadding="27" border="0" width="100%" height="20%">
       <tr align="center">
-        <td bgcolor="pink">
-          <div id="footer" style="height:50px; line-height:50px; background:#333; color:pink;">
+        <td bgcolor="yellow">
+          <div id="footer" style="height:50px; line-height:50px; background:#333; color:yellow;">
             Copyright &copy; 2019
             Designed by . . . . . . . .
           </div>
